7774758a5e7d14127c0b94a9d94a31c9257a8327
Ayan Pal <ayanpal58@gmail.com>
2026-01-01 14:30:44 +0530

    added two sum

---
+++ src/main/java/org/neet/code/Easy/array/TwoSum.java
@@ -0,0 +1,73 @@
+package org.neet.code.Easy.array;
+
+/*
+ * 1. Two Sum
+ * Easy
+ *
+ * Problem:
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ *
+ * Example 1:
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ *
+ * Example 2:
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ *
+ * Example 3:
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ *
+ * Constraints:
+ * 2 <= nums.length <= 10^4
+ * -10^9 <= nums[i] <= 10^9
+ * -10^9 <= target <= 10^9
+ * Only one valid answer exists.
+ *
+ * Follow-up: Can you come up with an algorithm that is less than O(n^2) time complexity?
+ */
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+        int c = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                return result;
+            }
+            map.put(nums[i], c++);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] result1 = twoSum(nums1, target1);
+        System.out.println("Result 1: [" + result1[0] + ", " + result1[1] + "]");
+
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] result2 = twoSum(nums2, target2);
+        System.out.println("Result 2: [" + result2[0] + ", " + result2[1] + "]");
+
+        int[] nums3 = {3, 3};
+        int target3 = 6;
+        int[] result3 = twoSum(nums3, target3);
+        System.out.println("Result 3: [" + result3[0] + ", " + result3[1] + "]");
+    }
+}
+
